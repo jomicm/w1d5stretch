03833590238ecec6d0ea9bf1f2e4d6b99c5abb78
jomicm <singularity.jmcm@gmail.com>
2019-09-20 22:05:27 -0400

    A more elegant solution

--- max-profit-elegant.js
+++ max-profit-elegant.js
@@ -1,19 +1,14 @@
 
 const maxProfit = function(input) {
   let maxProf = -1;
-  if (input.length > 2) {
-    // Divide
-    // While splitting using splice, theres is no need to look for the second half since input becomes the second half
-    let fh = input.splice(0, Math.ceil(input.length / 2));
+  if (input.length > 1) {
+    let fh = input.splice(0, Math.floor(input.length / 2));
     let dif = Math.max(...input) - Math.min(...fh);
-    // Update
-    maxProf = maxProfit > dif ? maxProfit : dif;
-    // Conquer
-    maxProfit(fh);
-    maxProfit(input);
-    return '\nThe max profit is: ' + maxProf + '\n';
-  }
+    maxProf = maxProf > dif ? maxProf : dif;
+    return Math.max(maxProf, maxProfit(fh), maxProfit(input));
+  } else  return -1;
 };
 
+console.log(maxProfit([45, 24, 35, 31, 40, 38, 11, 1, 99]));
 console.log(maxProfit([45, 24, 35, 31, 40, 38, 11]));
 console.log(maxProfit([200, 500, 1000, 700, 30, 400, 900, 400, 50]));
